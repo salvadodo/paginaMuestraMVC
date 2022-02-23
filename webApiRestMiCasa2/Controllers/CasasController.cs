4e0d5d7157fd709160c1df58242b6e17dda3055d
salvadodo <salvadfloz@gmail.com>
2022-02-23 14:50:24 -0600

    Crud completo de entidad Casas

--- webApiRestMiCasa2/Controllers/CasasController.cs
+++ webApiRestMiCasa2/Controllers/CasasController.cs
@@ -12,30 +12,40 @@ namespace webApiRestMiCasa2.Controllers
     public class CasasController : ApiController
     {
         // GET: api/Casas
+        [HttpGet]
+        [Route("api/Casas")]
         public List<Casas> Get()
         {
             return miCasaData.registrosCasas();
         }
 
         // GET: api/Casas/5
+        [HttpGet]
+        [Route("api/Casas/{id}")]
         public Casas Get(int id)
         {
             return miCasaData.selectCasa(id);
         }
 
         // POST: api/Casas
-        public bool Post([FromBody]Casas insCasa)
+        [HttpPost]
+        [Route("api/Casas")]
+        public bool Post([FromBody] Casas insCasa)
         {
             return miCasaData.insertarCasa(insCasa);
         }
 
         // PUT: api/Casas/5
-        public bool Put([FromBody]Casas modCasa)
+        [HttpPut]
+        [Route("api/Casas/{modCasa}")]
+        public bool Put([FromBody] Casas modCasa)
         {
             return miCasaData.modificarCasas(modCasa);
         }
 
         // DELETE: api/Casas/5
+        [HttpDelete]
+        [Route("api/Casas/{eliCasa}")]
         public bool Delete(int eliCasa)
         {
             return miCasaData.eliminarCasa(eliCasa);
