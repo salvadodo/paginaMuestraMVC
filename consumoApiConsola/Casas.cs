ce5fe21582d88b734fde3e38d819812c2cb46608
salvadodo <salvadfloz@gmail.com>
2022-03-10 01:09:38 -0600

    Prueba en consola del webapi

---
+++ consumoApiConsola/Casas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consumoApiConsola
+{
+    internal class Casas
+    {
+        public int idCasa { get; set; }
+        public string tipodeCasa { get; set; }
+        public string Ubicacion { get; set; }
+        public string Descripcion { get; set; }
+        public int? Dueño { get; set; }
+    }
+}
---
+++ consumoApiConsola/Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consumoApiConsola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient ClientHttp = new HttpClient();
+            ClientHttp.BaseAddress = new Uri("https://localhost:44325");
+            var request = ClientHttp.GetAsync("api/Casas").Result;
+            if(request.IsSuccessStatusCode)
+            {
+                var respuesta=request.Content.ReadAsStringAsync().Result;
+                var listado=JsonConvert.DeserializeObject<List<Casas>>(respuesta);
+                foreach(var item in listado)
+                {
+                    Console.WriteLine(item.Ubicacion);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
